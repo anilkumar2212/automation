c2e1190fcd8dc9dbabca53f9c47225523d2fdfd4
anilkumar <anilkumar.m.22121996@gmail.com>
2023-10-16 16:16:50 +0530

    changes commited in app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ def dvc_remote(product_name,comments):
     dvc_commands = [
     f'dvc add data/{product_name}',
     f'git add data/{product_name}.dvc',
+    'git add .dvc/config',
     f'git commit -m "{comments}"',
     'dvc push',
     'git push']
